dcb2cd5135f433e5fcbed400789f2416e38d1ad1
Cláudio Lima <crl.drums@gmail.com>
2021-09-07 17:10:43 -0300

    charts added

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="footer footer navbar-fixed-bottom mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/claudio-lima-101/" target="_blank" rel="noreferrer">Cláudio Lima</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+        <a className="navbar-brand" href="#">&nbsp; &nbsp;Cláudio's App </a>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link example</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link example</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">App treino</a>
+          </li>
+        </ul>
+      </nav>
+    );
+}
+export default NavBar;
