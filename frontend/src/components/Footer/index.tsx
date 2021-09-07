0f42e3eea273671d310001a4795b4ddb73d276ed
Cláudio Lima <crl.drums@gmail.com>
2021-09-07 17:50:20 -0300

    contacts added

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,8 +3,8 @@ function Footer() {
     <footer className="footer footer navbar-fixed-bottom mt-auto py-3 bg-dark">
       <div className="container">
         <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/claudio-lima-101/" target="_blank" rel="noreferrer">Cláudio Lima</a></p>
-        <p className="text-light"><small><strong>Semana Spring React</strong><br />
-          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+        <p className="text-light"><small><strong>Contatos:</strong><br />
+          Instagram: <a href="https://instagram.com/claudio.lima.crl" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
       </div>
     </footer>
   );
