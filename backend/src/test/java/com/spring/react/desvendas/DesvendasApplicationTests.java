d049bbf18ff98ed0226404865310be48f40a93b5
Cláudio Lima <crl.drums@gmail.com>
2021-09-07 09:42:09 -0300

    first commit

---
+++ backend/src/test/java/com/spring/react/desvendas/DesvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.react.desvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
