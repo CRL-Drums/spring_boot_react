653525d97f0651dbd299f9effbb315c7ab3d09cb
Cláudio Lima <crl.drums@gmail.com>
2021-09-11 16:59:58 -0300

    DB integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,33 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number
+    visited: number
+    deals: number
+    amount: number
+    date: string
+    seller: Seller
+}
+
+export type SalePage = {
+    content?: Sale[]
+    last: boolean
+    totalPages: number
+    totalElements: number
+    size?: number
+    first: boolean
+    number: number
+    numberOfElements?: number
+    empty?: boolean
+}
+
 export type SaleSum = {
-    sellerName: string,
+    sellerName: string
     sum: number
 }
 
 export type SaleSuccess = {
-    sellerName: string,
-    visited: number,
+    sellerName: string
+    visited: number
     deals: number
 }
