36bcc53c36fff6fe50717b5201c9261bf5af8887
Cláudio Lima <crl.drums@gmail.com>
2021-09-11 15:52:04 -0300

    donnut chart integration with api

--- frontend/src/assets/DonutChart/index.tsx
+++ frontend/src/assets/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,37 +11,41 @@ type ChartData = {
 
 function DonutChart() {
 
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+
     // FORMA ERRADA
-    let chartData : ChartData = { labels: [], series: []};
+    //let chartData : ChartData = { labels: [], series: []};
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const dataLabels = data.map(x => x.sellerName);
-            const dataSeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const dataLabels = data.map(x => x.sellerName);
+                const dataSeries = data.map(x => x.sum);
 
-            chartData = { labels: dataLabels, series: dataSeries };
-            console.log(chartData);
-        });
+                setChartData({ labels: dataLabels, series: dataSeries });
+                console.log(chartData);
+            });
+    }, []);
 
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Carlos', 'Brenice', 'Silvia', 'Rafaela', 'Cláudio']
     //}
-    
+
     const options = {
         legend: {
             show: true
         }
     }
     return (
-        <Chart 
-            options={{ ...options, labels: chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
         />
     );
-  }
-  
-  export default DonutChart;
+}
+
+export default DonutChart;
