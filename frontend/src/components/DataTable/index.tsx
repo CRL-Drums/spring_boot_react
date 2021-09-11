5f9b68ff259ffdc821d6be951bc984ebc94ec2af
Cláudio Lima <crl.drums@gmail.com>
2021-09-11 18:19:18 -0300

    fixed?

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -17,7 +17,7 @@ function DataTable() {
     })
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
+        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
             .then(response => {
                 setPage(response.data)
             })
