386d93cd9a1f71e693e64ad09efaea6fda61edee
Claudio Lima <80592108+CRL-Drums@users.noreply.github.com>
2021-09-12 17:39:30 -0300

    Fix buttom

--- frontend/src/components/pagination/index.tsx
+++ frontend/src/components/pagination/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 function Pagination( { page, onPageChange } : Props) {
 
     return (
-        <div className="row d-flex justify-content-center">
+        <div className="d-flex justify-content-center">
             <nav>
                 <ul className="pagination">
                     <li className={`page-item ${page.first ? 'disabled' : ''} `}>
